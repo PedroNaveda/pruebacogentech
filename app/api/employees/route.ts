b3df5e3566f12ea287e51270ad2fc36cb81b0f01
PedroNaveda <naveda.pedro@gmail.com>
2023-11-28 22:51:31 -0500

    employeesHierarchy

--- app/api/employees/route.ts
+++ app/api/employees/route.ts
@@ -3,13 +3,47 @@ import { PrismaClient } from '@prisma/client';
 
 const prisma = new PrismaClient();
 
-export async function GET() {
-  const employees = await prisma.employee.findMany();
+interface Employee {
+  id: number;
+  name: string;
+  lastname: string;
+  email: string;
+  phone: string;
+  supervisorid: number | null;
+  children?: Employee[];
+}
 
-  return NextResponse.json({
-    data: employees,
-    message: 'Getting Employees',
-  });
+export async function GET() {
+  try {
+    // Obtener la estructura jerárquica de los empleados
+    const hierarchy = await prisma.employee.findMany({
+      orderBy: { supervisorid: 'asc' },
+    });
+
+    // Función para construir la estructura jerárquica
+    const buildHierarchy = (
+      employees: Employee[],
+      supervisorid: number | null
+    ): Employee[] => {
+      return employees
+        .filter((employee) => employee.supervisorid === supervisorid)
+        .map((employee) => ({
+          ...employee,
+          children: buildHierarchy(employees, employee.id),
+        }));
+    };
+
+    // Construir la estructura jerárquica inicial
+    const hierarchicalEmployees = buildHierarchy(hierarchy, null);
+
+    return NextResponse.json({
+      data: hierarchicalEmployees,
+      message: 'Getting Employees Hierarchy',
+    });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.error();
+  }
 }
 
 export async function POST(request: Request) {
